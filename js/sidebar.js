f47d1f71a0174311576097283b142648c2af7c4c
vdhakad318-eng <vdhakad318@gmail.com>
2026-07-07 17:21:49 +0530

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,12 +2,21 @@ const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.querySelector(".sidebar");
 
 menuBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("show");
+
+    if (window.innerWidth <= 900) {
+        // Mobile
+        sidebar.classList.toggle("show");
+    } else {
+        // Desktop
+        sidebar.classList.toggle("collapsed");
+    }
+
 });
 
 document.addEventListener("click", (e) => {
 
     if (
+        window.innerWidth <= 900 &&
         !sidebar.contains(e.target) &&
         !menuBtn.contains(e.target)
     ) {
