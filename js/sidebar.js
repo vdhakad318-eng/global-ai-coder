0ff36214e1b19fc0664503a6fe5d4cbe3d4c4b5a
vdhakad318-eng <vdhakad318@gmail.com>
2026-07-07 16:38:45 +0530

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,17 @@
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.querySelector(".sidebar");
 
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("show");
+});
+
+document.addEventListener("click", (e) => {
+
+    if (
+        !sidebar.contains(e.target) &&
+        !menuBtn.contains(e.target)
+    ) {
+        sidebar.classList.remove("show");
+    }
+
+});
